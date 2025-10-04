d9e83c6ecfd97f7f853acdb9d3d79922b1368428
penknife6153 <ardahal@uno.edu>
2025-10-03 21:16:55 -0500

    Add explanation for restricted blocks in two knights problem.

--- two-knights.cpp
+++ two-knights.cpp
@@ -7,7 +7,7 @@ typedef long long ll;
 ll twoKnights(ll n) {
     ll numberOfBlocks = n * n;
     ll allPossibleCombinations = numberOfBlocks * (numberOfBlocks - 1) / 2;    // nC2; not nP2 because the two knights are identical
-    ll restrictedBlocks = 4 * (n - 1) * (n - 2);
+    ll restrictedBlocks = 4 * (n - 1) * (n - 2); // there are (n-1)*(n-2) ways to arrange 3x2 and 2x3 blocks (each). Combined, that's 2(n-1)(n-2). We multiply by 2 because within every single 2x3 rectangle, there are exactly two pairs of squares where knights can attack each other.
 
     return allPossibleCombinations - restrictedBlocks;
 }
