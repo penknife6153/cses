0d50e3dcbcf73776e8d6bcfb8ea038f550bff9ad
penknife6153 <ardahal@uno.edu>
2025-10-04 16:34:28 -0500

    Slightly more efficient gray code solution.
    Refactor grayCode function to optimize memory usage, reduce unnecessary copying operations, and improve readability. Added base case for n=0.

--- gray-code.cpp
+++ gray-code.cpp
@@ -5,22 +5,26 @@ typedef long long ll;
 
 vector<string> grayCode(ll n) {
     // base case
+    if (n == 0) {
+        return {""};
+    }
     if (n == 1) {
         return {"0","1"};
     }
     
-    vector<string> prevList = grayCode(n-1);
+    vector<string> prevList, combinedList;
 
-    vector<string> prevListReversed(prevList.rbegin(), prevList.rend());
-
-    vector<string> combinedList;
+    prevList = grayCode(n-1); // get previous gray code sequence recursively
+    combinedList.reserve(2*prevList.size()); // reserve memory
 
     for (string &item: prevList) {
         combinedList.push_back("0" + item);
     }
-    for (string &item: prevListReversed) {
-        combinedList.push_back("1" + item);
+
+    for (ll i = prevList.size() - 1; i >= 0; i--) { // copying reversed list into a new var requires 2^(n-1) operations, so we simply use reverse indexing instead.
+        combinedList.push_back("1" + prevList[i]);
     }
+    
     return combinedList;
 }
 
